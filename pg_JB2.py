f4f7d73189603d0bf8d6e81a83fca1f4e2822b7c
jb223 <33520860+jb223@users.noreply.github.com>
2018-01-11 09:52:32 -0500

    Add files via upload

---
+++ pg_JB2.py
@@ -0,0 +1,6 @@
+import pyautogui as pg
+import time
+
+pg.hotkey('ctrl','winleft','d')
+pg.hotkey('winleft')
+pg.typewrite('chrome
---
+++ pg_jb.py
@@ -0,0 +1,5 @@
+import pyautogui as pg
+
+pg.click(910,33)
+pg.typewrite('danny devito\n')
+             
